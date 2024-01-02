d8f1dabd5b178a1d40a55ce3807be0c767d2555c
Ike Addy <ikaddy05@gmail.com>
2024-01-01 19:10:26 -0500

    Added HTML skeleton Created array to contain books Created book constructor Created function to add books to library

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, status) {
+    this.title = title,
+    this.author = author,
+    this.status = status,
+}
+
+function addBookToLibrary() {
+
+}
